406205d4a35761634e96591c966081eea826f419
NickHAnderson019 <nick@thalana.co.za>
2020-05-08 10:29:00 +0200

    Added basics of Telegram Bot

--- Nick/learning.py
+++ Nick/learning.py
@@ -1,3 +1,56 @@
 # This script will be used for learning the functionality of a Telegram Bot
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import logging
+import threading
 
-print("Hello World!")
+TOKEN = '1082507548:AAH6h7DxP5WdUwfb9i9Z7EOMjKKSSuNU1Kg'
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+# /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+# echo
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+# caps
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+# shutdown Bot
+def shutdown():
+    updater.stop()
+    updater.is_idle = False
+
+def stop(bot, update):
+    threading.Thread(target=shutdown).start()
+
+updater.dispatcher.add_handler(CommandHandler('stop', stop))
+
+# unknown
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
+
+updater.idle()
